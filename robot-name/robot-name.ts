bdc29126d8db2f1022eaa9b32a517726c874f8ce
Rob Prouse <rob@prouse.org>
2021-04-17 19:48:38 -0400

    checkpointing Robot Name. Working, but slow

---
+++ robot-name/robot-name.ts
@@ -0,0 +1,50 @@
+export default class Robot {
+  private static allNames: Array<string> = Robot.createAllPossibleNames();
+  private _name: string;
+
+  constructor() {
+    this._name = Robot.createUniqueName();
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public resetName(): void {
+    this._name = Robot.createUniqueName();
+  }
+
+  public static releaseNames(): void {
+    Robot.allNames = Robot.createAllPossibleNames();
+  }
+
+  private static createUniqueName(): string {
+    const i = Robot.getRandomInt(0, Robot.allNames.length);
+    const name = Robot.allNames[i];
+    Robot.allNames.splice(i, 1);  // delete from names
+    return name;
+  }
+
+  private static createAllPossibleNames() : Array<string> {
+    const A = 'A'.charCodeAt(0);
+    const allNames = new Array<string>();
+    for(let l1 = 0; l1 < 26; l1++) {
+      for(let l2 = 0; l2 < 26; l2++) {
+        for(let n1 = 0; n1 < 10; n1++) {
+          for(let n2 = 0; n2 < 10; n2++) {
+            for(let n3 = 0; n3 < 10; n3++) {
+              allNames.push(`${String.fromCharCode(A + l1)}${String.fromCharCode(A + l2)}${n1}${n2}${n3}`);
+            }
+          }
+        }
+      }
+    }
+    return allNames;
+  }
+
+  private static getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+}
