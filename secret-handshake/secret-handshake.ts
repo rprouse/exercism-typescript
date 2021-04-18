5115429231f0c07c6210eed8184a398ef264b0c6
Rob Prouse <rob@prouse.org>
2021-04-18 11:03:45 -0400

    Simplify with a filter and left shift

--- secret-handshake/secret-handshake.ts
+++ secret-handshake/secret-handshake.ts
@@ -1,29 +1,12 @@
-const WINK         = 0b0001;
-const DOUBLE_BLINK = 0b0010;
-const CLOSE_EYES   = 0b0100;
-const JUMP         = 0b1000;
-const REVERSE      = 0b00010000;
+const REVERSE = 0b00010000;
+const handshake = ['wink', 'double blink', 'close your eyes', 'jump'];
 
 export default class HandShake {
   private _commands: string[];
 
   constructor(num: number) {
-    this._commands = [];
-    if (num & WINK) {
-      this._commands.push('wink');
-    }
-    if (num & DOUBLE_BLINK) {
-      this._commands.push('double blink');
-    }
-    if (num & CLOSE_EYES) {
-      this._commands.push('close your eyes');
-    }
-    if (num & JUMP) {
-      this._commands.push('jump');
-    }
-    if (num & REVERSE) {
-      this._commands.reverse();
-    }
+    this._commands = handshake.filter((_, index) => 1 << index & num);
+    if (num & REVERSE) this._commands.reverse();
   }
 
   commands = (): string[] => this._commands;
