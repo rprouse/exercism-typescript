d0bc8a2db914a41337a2ef1cbc54ab7f04ba86ea
Rob Prouse <rob@prouse.org>
2021-04-28 22:03:52 -0400

    Solve simple cipher

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,11 +1,38 @@
+const A = 'a'.charCodeAt(0);
+
+const getRandomInt = (max: number): number =>
+  Math.floor(Math.random() * Math.floor(max));
+
+const randomKey = (): string =>
+Array.from(Array(100).keys())
+    .map(_ => String.fromCharCode(A + getRandomInt(26)))
+    .join('');
+
 class SimpleCipher {
-  encode(/* Parameters go here */) {
-    // Your code here
+  key: string;
+
+  constructor(key?: string) {
+    this.key = key ?? randomKey();
   }
 
-  decode(/* Parameters go here */) {
-    // Your code here
-  }
+  encode = (text: string): string =>
+    text.split('')
+      .map((c, i) => {
+        const enc = c.charCodeAt(0) +
+          this.key.charCodeAt(i % this.key.length) - 2 * A;
+        return String.fromCharCode(enc % 26 + A);
+      })
+      .join('');
+
+  decode = (text: string): string =>
+    text.split('')
+      .map((c, i) => {
+        let dec = c.charCodeAt(0) - this.key.charCodeAt(i % this.key.length);
+        if (dec < 0)
+          dec += 26;
+        return String.fromCharCode(A + dec);
+      })
+      .join('');
 }
 
 export default SimpleCipher
