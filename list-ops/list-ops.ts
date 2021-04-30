af5e1e9d0a3fe7d8f859c56afb19ea89068212eb
Rob Prouse <rob@prouse.org>
2021-04-30 10:21:25 -0400

    Solve listops

--- list-ops/list-ops.ts
+++ list-ops/list-ops.ts
@@ -9,41 +9,63 @@ export default class List<T> {
     this.values = values ?? [];
   }
 
+  private add(item: T): void {
+    this.values.push(item);
+  }
+
   length(): number {
     return this.values.length;
   }
 
   append(list: List<T>): List<T> {
     const result = new List(this.values);
-    list.values.forEach((item) => result.values.push(item));
+    list.values.forEach((item) => result.add(item));
     return result;
   }
 
   concat(list: List<T> | List<List<T>>): List<T> {
     const result = new List<T>(this.values);
     list.values.forEach((item: T | List<T>) => {
-      item instanceof List ? result.append(item) : result.values.push(item);
+      item instanceof List ? result.append(item) : result.add(item);
     });
     return result;
   }
 
-  filter(_: FilterOp<T>): List<T> {
-    return this;
+  filter(filterOp: FilterOp<T>): List<T> {
+    const result = new List<T>();
+    this.values.forEach(item => {
+      if (filterOp(item)) result.add(item);
+    });
+    return result;
   }
 
-  map(_: MapOp<T>): List<T> {
-    return this;
+  map(mapOp: MapOp<T>): List<T> {
+    const result = new List<T>();
+    this.values.forEach(item => {
+      result.add(mapOp(item));
+    });
+    return result;
   }
 
-  foldl(_: FoldOp<T>, accumulator: T): T {
-    return _(accumulator, this.values[0]);
+  foldl(foldOp: FoldOp<T>, accumulator: T): T {
+    this.values.forEach(item => {
+      accumulator = foldOp(accumulator, item);
+    });
+    return accumulator;
   }
 
-  foldr(_: FoldOp<T>, accumulator: T): T {
-    return _(accumulator, this.values[0]);
+  foldr(foldOp: FoldOp<T>, accumulator: T): T {
+    for (let idx = this.values.length - 1; idx >= 0; idx--) {
+      accumulator = foldOp(accumulator, this.values[idx]);
+    };
+    return accumulator;
   }
 
   reverse(): List<T> {
-    return this;
+    const result = new List<T>();
+    for (let idx = this.values.length - 1; idx >= 0; idx--) {
+      result.add(this.values[idx]);
+    }
+    return result;
   }
 }
